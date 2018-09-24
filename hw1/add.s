75c79f51b82d0843329b85b708c300a0eef88d6a
Anthony Donatelli <adonatel@stevens.edu>
2018-09-24 12:46:29 -0400

    Homework1

---
+++ hw1/add.s
@@ -0,0 +1,5 @@
+
+.global _Z3addii
+_Z3addii:
+	add	r0, r0, r1
+	bx	lr
---
+++ hw1/hw1.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+extern int add(int a, int b);
+
+int main() {
+	cout << add(2,3) << '\n';
+}
