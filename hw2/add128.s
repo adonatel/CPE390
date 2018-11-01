1800f94bb0d61055b96ecf4b9551122e6f83dd7b
Anthony Donatelli <adonatel@stevens.edu>
2018-11-01 16:04:00 -0400

    HW2 Complete

---
+++ hw2/add128.s
@@ -0,0 +1,46 @@
+.global _Z6add128RK6BigNumS1_
+_Z6add128RK6BigNumS1_:
+	push {r3,r4,r5}
+	ldr r3,[r1,#12]
+	ldr r4,[r2,#12]
+	adds r5,r3,r4
+	str r5,[r0,#12]
+	ldr r3,[r1,#8]
+        ldr r4,[r2,#8]
+        adcs r5,r3,r4
+        str r5,[r0,#8]
+	ldr r3,[r1,#4]
+        ldr r4,[r2,#4]
+        adcs r5,r3,r4
+        str r5,[r0,#4]
+	ldr r3,[r1]
+        ldr r4,[r2]
+        adcs r5,r3,r4
+        str r5,[r0]
+	pop {r3,r4,r5}
+	bx lr
+
+
+
+
+.global _ZplRK6BigNumS1_
+_ZplRK6BigNumS1_:
+	push {r3,r4,r5}
+        ldr r3,[r1,#12]
+        ldr r4,[r2,#12]
+        adds r5,r3,r4
+        str r5,[r0,#12]
+        ldr r3,[r1,#8]
+        ldr r4,[r2,#8]
+        adcs r5,r3,r4
+        str r5,[r0,#8]
+        ldr r3,[r1,#4]
+        ldr r4,[r2,#4]
+        adcs r5,r3,r4
+        str r5,[r0,#4]
+        ldr r3,[r1]
+        ldr r4,[r2]
+        adcs r5,r3,r4
+        str r5,[r0]
+        pop {r3,r4,r5}
+	bx lr
---
+++ hw2/cppadd128.cc
@@ -0,0 +1,20 @@
+#include "BigNum.hh"
+
+BigNum operator + (const BigNum& left, const BigNum& right) {
+  BigNum ans;
+	ans.d = left.d + right.d; // look up how to do carry ARM   ADD WITH CARRY ADC
+	ans.c = left.c + right.c;// + carry;
+	ans.b = left.b + right.b;// + carry;
+	ans.a = left.a + right.a;// + carry;
+	return ans;
+}
+
+BigNum add128(const BigNum& left, const BigNum& right)
+{
+	BigNum ans;
+	ans.a=0;
+	ans.b=0;
+	ans.c=0;
+	ans.d=0;
+	return ans;
+}
