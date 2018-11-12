1d6c9f57c207ba7de0a805aba5753ccc3ab8ade4
adonatel <43252460+adonatel@users.noreply.github.com>
2018-11-12 13:22:24 -0500

    Add files via upload

---
+++ Arduino/Anthony_Jeriel_SoundGen/SoundGen/SoundGen.ino
@@ -0,0 +1,15 @@
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int n=264000;
+  for (int i = 0; i < n; i++) {
+   int x=128 + 128*sin(110*2*3.14*i)+128*sin(132*2*3.14*i)+128*sin(165*2*3.14*i);
+   analogWrite(DAC0,x);
+   delayMicroseconds(500);
+}
+  
+}
